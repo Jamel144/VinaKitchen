9bfb0c40758342d9b8b63e3e4728f4c38808fe3d
Jamel Johnson <jameljohnson@jamels-mbp-2.home>
2024-04-08 21:49:10 +0100

    made alterations to mobile screen and cleared some files

---
+++ redo.js
@@ -0,0 +1,11 @@
+
+function showSideBar(){
+  const mobileNav = document.querySelector(".mobileNav")
+  mobileNav.style.display = "flex"
+}
+
+  function hideSideBar(){
+    const mobileNav = document.querySelector(".mobileNav")
+  mobileNav.style.display = "none"
+  }
+   
